a87485937648d9ec90b798b9ad9b2a70d2e8099d
pedrosalim <pedrof.salim@gmail.com>
2022-10-22 19:14:16 -0300

    finish

--- Projeto1/monografias/models.py
+++ Projeto1/monografias/models.py
@@ -1,6 +1,36 @@
 from datetime import datetime
 from django.db import models
 
+class Advisor(models.Model):
+    name = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    lattes = models.URLField()
+    google_scholar = models.URLField()
+    research_gate = models.URLField()
+    linkedin = models.URLField()
+    orcid = models.URLField()
+    github = models.URLField()
+
+    def __str__(self):
+        return (self.name)
+
+class CoAdvisor(models.Model):
+    name = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    lattes = models.URLField()
+    google_scholar = models.URLField()
+    research_gate = models.URLField()
+    linkedin = models.URLField()
+    orcid = models.URLField()
+    github = models.URLField()
+
+    def __str__(self):
+        return (self.name)
+
 class Author(models.Model):
     name = models.CharField(max_length=50)
     course = models.CharField(max_length=50)
@@ -13,59 +43,20 @@ class Author(models.Model):
     orcid = models.URLField()
     github = models.URLField()
 
-class CoAuthor(models.Model):
-    name = models.CharField(max_length=50)
-    course = models.CharField(max_length=50)
-    university = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    lattes = models.URLField()
-    google_scholar = models.URLField()
-    research_gate = models.URLField()
-    linkedin = models.URLField()
-    orcid = models.URLField()
-    github = models.URLField()
-
-class Student(models.Model):
-    name = models.CharField(max_length=50)
-    course = models.CharField(max_length=50)
-    university = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    lattes = models.URLField()
-    google_scholar = models.URLField()
-    research_gate = models.URLField()
-    linkedin = models.URLField()
-    orcid = models.URLField()
-    github = models.URLField()
+    def __str__(self):
+        return (self.name)
 
 class Monography(models.Model):
     title = models.CharField(max_length=50)
-    author = models.ForeignKey(Student, on_delete=models.CASCADE)
-    advisor = models.ForeignKey(Author, on_delete=models.CASCADE)
-    co_advisor = models.ForeignKey(CoAuthor, on_delete=models.CASCADE)
-    date = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    advisor = models.ForeignKey(Advisor, on_delete=models.CASCADE)
+    co_advisor = models.ForeignKey(CoAdvisor, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add = True)
     summary = models.CharField(max_length=100)
     key_words = models.CharField(max_length=100)
     university = models.CharField(max_length=80)
     course = models.CharField(max_length=50)
     monography = models.URLField()
 
-class MonographyAuthor(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    monography = models.ForeignKey(Monography, on_delete=models.CASCADE)
-
     def __str__(self):
-        return f'{self.author} -> {self.monography}'
-
-class MonographyCoAuthor(models.Model):
-    co_author = models.ForeignKey(CoAuthor, on_delete=models.CASCADE)
-    monography = models.ForeignKey(Monography, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.co_author} -> {self.monography}'
-
-class MonographyStudent(models.Model):    
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    monography = models.ForeignKey(Monography, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.student} -> {self.monography}'
+        return (self.title)
--- Projeto1/monografias/views.py
+++ Projeto1/monografias/views.py
@@ -1,33 +1,57 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.db.models import Q
-from .models import Monography, Author, CoAuthor, Student
+from .models import Monography, Advisor, CoAdvisor, Author
 
 from django.views import View
 
 # Create your views here.
-class IndexView(View):
-    def get(self, request):
-        return render(request, 'monografias/index.html')
+class AdvisorView(View):
+  def get(self, request):
+    return render(request, 'monografias/create-advisor.html')
+  
+  def post(self, reqeust):
+    data = request.POST
+    Advisor.objects.create(
+      name=data['name'], 
+      course=data['course'], 
+      university=data['university'], 
+      email=data['email'], 
+      lattes=data['lattes'], 
+      google_scholar=data['google_scholar'], 
+      research_gate=data['research_gate'], 
+      linkedin=data['linkedin'], 
+      orcid=data['orcid'], 
+      github=data['github']
+    )
+    return redirect('index')
 
-class CreateAuthorView(View):
-    def get(self, request):
-        return render(request, 'monografias/create-author.html')
-
-class CreateCoAuthorView(View):
-    def get(self, request):
-        return render(request, 'monografias/create-co-author.html')
-
-class CreateStudentView(View):
-    def get(self, request):
-        return render(request, 'monografias/create-student.html')
+class CoAdvisorView(View):
+  def get(self, request):
+    return render(request, 'monografias/create-co-advisor.html')
+  
+  def post(self, reqeust):
+    data = request.POST
+    CoAdvisor.objects.create(
+      name=data['name'], 
+      course=data['course'], 
+      university=data['university'], 
+      email=data['email'], 
+      lattes=data['lattes'], 
+      google_scholar=data['google_scholar'], 
+      research_gate=data['research_gate'], 
+      linkedin=data['linkedin'], 
+      orcid=data['orcid'], 
+      github=data['github']
+    )
+    return redirect('index')
 
 class AuthorView(View):
   def get(self, request):
     return render(request, 'monografias/create-author.html')
   
   def post(self, reqeust):
-    data = reqeust.POST
+    data = request.POST
     Author.objects.create(
       name=data['name'], 
       course=data['course'], 
@@ -40,65 +64,43 @@ class AuthorView(View):
       orcid=data['orcid'], 
       github=data['github']
     )
-    redirect('index')
-
-class CoAuthorView(View):
-  def get(self, request):
-    return render(request, 'monografias/create-co-author.html')
-  
-  def post(self, reqeust):
-    data = reqeust.POST
-    CoAuthor.objects.create(
-      name=data['name'], 
-      course=data['course'], 
-      university=data['university'], 
-      email=data['email'], 
-      lattes=data['lattes'], 
-      google_scholar=data['google_scholar'], 
-      research_gate=data['research_gate'], 
-      linkedin=data['linkedin'], 
-      orcid=data['orcid'], 
-      github=data['github']
-    )
-    redirect('index')
-
-class StudentView(View):
-  def get(self, request):
-    return render(request, 'monografias/create-student.html')
-  
-  def post(self, reqeust):
-    data = reqeust.POST
-    Student.objects.create(
-      name=data['name'], 
-      course=data['course'], 
-      university=data['university'], 
-      email=data['email'], 
-      lattes=data['lattes'], 
-      google_scholar=data['google_scholar'], 
-      research_gate=data['research_gate'], 
-      linkedin=data['linkedin'], 
-      orcid=data['orcid'], 
-      github=data['github']
-    )
-    redirect('index')
+    return redirect('index')
 
 class MonographyView(View):
   def get(self, request):
-    author = Author.objects.all()
-    co_author = CoAuthor.objects.all()
-    student = Student.objects.all()
-    return render(request, 'monografias/monography.html', {
-      'author': author, 'co_author': co_author, 'student': student
+    advisors = Advisor.objects.all()
+    co_advisors = CoAdvisor.objects.all()
+    authors = Author.objects.all()
+    return render(request, 'monografias/create-monography.html', {
+      'authors': authors, 'advisors': advisors, 'co_advisors': co_advisors
     })
   def post(self, request):
-    pass
+    data = request.POST
+    advisor = Advisor.objects.get(pk=data['advisor'])
+    co_advisor = CoAdvisor.objects.get(pk=data['co_advisor'])
+    author = Author.objects.get(pk=data['author'])
+
+    Monography.objects.create(
+      title=data['title'], 
+      author=advisor, 
+      advisor=advisor, 
+      co_advisor=co_advisor, 
+      date=data['date'],
+      google_scholar=data['google_scholar'], 
+      summary=data['summary'], 
+      key_words=data['linkedin'], 
+      university=data['university'], 
+      course=data['course']
+      monography=data['monography']
+    )
+    return redirect('index')
 
 class SearchMonographyView(View):
   def get(self, request):
-    return render(request, 'index.html')
+    return render(request, 'monografias/index.html')
 
   def post(self, request):
     search = request.POST['search']
     monographys = Monography.objects.filter(Q(title__icontains=search) | Q(university__icontains=search) | Q(summary__icontains=search))
 
-    return render(request, 'monografias/search.html', { "monographys": monographys })
+    return render(request, 'monografias/index.html', { "monographys": monographys })
